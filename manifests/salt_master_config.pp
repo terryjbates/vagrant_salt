80bcf7bac753a2f1d4722a576e2f59adeba2976b
terryjbates <terryjbates@gmail.com>
2014-10-24 15:50:07 -0700

    Create salt_master_config.pp

---
+++ manifests/salt_master_config.pp
@@ -0,0 +1,46 @@
+# Added global path
+Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
+
+# Get fresh packages
+exec { "update_packages":
+  command => "apt-get -y update",
+}
+# Install git for DVCS
+package { "git":
+  ensure => present,
+  require => Exec[ "update_packages" ]
+}
+
+
+exec { "install_python_sw_props":
+  command => "sudo apt-get -y install python-software-properties",
+  require => Exec[ "update_packages" ],
+}
+
+exec { "add_apt_repo":
+  command => "sudo apt-get -y update && sudo add-apt-repository ppa:saltstack/salt",
+  require => Exec[ "update_packages" ],
+}
+
+exec { "install_salt_master":
+  command => "sudo apt-get -y update && sudo apt-get -y install salt-master",
+  require => Exec[ "add_apt_repo" ],
+}
+
+exec { "install_python":
+  command => "sudo apt-get -y update && sudo apt-get -y install python python-dev python-setuptools",
+  require => Exec[ "update_packages"],
+}
+
+exec { "add_python_packages":
+  command => "sudo /usr/bin/easy_install pyzmq jinja2 pycrypto pyyaml m2crypto msgpack-python",
+  require => Exec[ "install_python"],
+}
+
+exec{ "set_hostname":
+  command => "sudo hostname salt-master",
+}
+
+exec{ "copy_etc_hosts":
+  command => "sudo cp /vagrant/etc_hosts /etc/hosts",
+}
